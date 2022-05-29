fa71b35be4a8d9a9abce46c3a0637391a99768e2
TankistPro <nakol.nayk@mail.ru>
2022-05-29 21:56:50 +0300

    fix baseUrl

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios, {AxiosRequestConfig} from 'axios';
 
-const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:5500/" : "http://tankistpro-food.ru:5500/"
+const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:5500/" : "https://tankistpro-food.ru:5500/"
 
 const api = axios.create({
     baseURL: baseUrl,
