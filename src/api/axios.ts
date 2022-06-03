46dda9d0e34926a8b3bb20ec4663774ccf697bf2
TankistPro <nakol.nayk@mail.ru>
2022-06-04 00:21:31 +0300

    test

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,18 +2,21 @@ import axios, {AxiosRequestConfig} from 'axios';
 
 // const baseUrl = process.env.NODE_ENV !== "development" ? "https://tankistpro-food.ru" : "";
 
-const createOptionsDevelopment = {
-    withCredentials: true,
-}
+// const createOptionsDevelopment = {
+//     withCredentials: true,
+// }
+//
+// const createOptionsProduction = {
+//     baseURL: "https://tankistpro-food.ru",
+//     withCredentials: true,
+// }
+//
+// const createOptions = process.env.NODE_ENV !== "development" ? createOptionsProduction : createOptionsDevelopment;
 
-const createOptionsProduction = {
+const api = axios.create({
     baseURL: "https://tankistpro-food.ru",
     withCredentials: true,
-}
-
-const createOptions = process.env.NODE_ENV !== "development" ? createOptionsProduction : createOptionsDevelopment;
-
-const api = axios.create(createOptions);
+});
 
 api.interceptors.request.use((config: AxiosRequestConfig) => {
     config.headers!.Authorization = localStorage.getItem('access') || '';
