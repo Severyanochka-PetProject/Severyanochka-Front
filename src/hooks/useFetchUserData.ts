eb73ce1e0996d87f9a3f8ae8680fac56134da3bf
TankistPro <nakol.nayk@mail.ru>
2022-06-04 21:06:27 +0300

    add console.log

--- src/hooks/useFetchUserData.ts
+++ src/hooks/useFetchUserData.ts
@@ -8,7 +8,7 @@ export default function useFetchUserData() {
     useEffect(() => {
         (async function fetchUserData () {
             const response = await AuthService.refresh();
-
+            console.log(response);
             localStorage.setItem('access', response.data)
 
             const { data } = await AuthService.me()
