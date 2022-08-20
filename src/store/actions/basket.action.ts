1463772b7bceeced9321633b5f10445446137db0
TankistPro <nakol.nayk@mail.ru>
2022-08-20 22:01:31 +0300

    Добавлено сохранение корзины в LocalStorage для неавторизованных пользователей

--- src/store/actions/basket.action.ts
+++ src/store/actions/basket.action.ts
@@ -1,12 +1,19 @@
 import basketService from "../../services/basketService";
 import {INIT_USER_BASKET} from "../reducers/basketSlice";
+import {BasketProduct} from "../../domain/Basket.domain";
 
 export const fetchUserBasket = () => {
     return async (dispatch : any, state: any) => {
         const { user } = state();
+        // TODO: Учитывать при входе в ЛК данные из Корзины в LocalStorage
+        const localBasket : BasketProduct[] = JSON.parse(localStorage.getItem('user_basket') || '[]');
 
-        const basket = await basketService.getUserBasket(user.user.vk_user_id || user.user.id_user);
+        if (user.isAuth) {
+            const basket = await basketService.getUserBasket(user.user.vk_user_id || user.user.id_user);
 
-        dispatch(INIT_USER_BASKET(basket.data.basket))
+            dispatch(INIT_USER_BASKET(basket.data.basket))
+        } else {
+            dispatch(INIT_USER_BASKET(localBasket))
+        }
     }
 }
