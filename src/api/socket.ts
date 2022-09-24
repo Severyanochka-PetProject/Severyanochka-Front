3528941e949f5757782c93491232d97e9dc91fa7
TankistPro <nakol.nayk@mail.ru>
2022-09-24 23:36:18 +0300

    fix

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const ENDPOINT = "http://localhost:5605/";
+const ENDPOINT = "https://tankistpro-food.ru/wss";
 
 export const socket = io(ENDPOINT, {
     withCredentials: true,
